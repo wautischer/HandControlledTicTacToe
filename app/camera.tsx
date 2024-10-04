21509801957fcce1766f89c5490cde811d5f0952
Laurin Wautischer <wautischer@outlook.com>
2024-10-04 12:13:07 +0200

    display camer on index.tsx

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,22 +1,17 @@
-;import PhotoPreviewSection from '@/components/PhotoPreviewSection';
-import { AntDesign } from '@expo/vector-icons';
 import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
 import { useRef, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Camera() {
-  const [facing, setFacing] = useState<CameraType>('back');
+  const [facing, setFacing] = useState<CameraType>('front');
   const [permission, requestPermission] = useCameraPermissions();
-  const [photo, setPhoto] = useState<any>(null);
   const cameraRef = useRef<CameraView | null>(null);
 
   if (!permission) {
-    // Camera permissions are still loading.
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
@@ -25,39 +20,9 @@ export default function Camera() {
     );
   }
 
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-  }
-
-  const handleTakePhoto =  async () => {
-    if (cameraRef.current) {
-        const options = {
-            quality: 1,
-            base64: true,
-            exif: false,
-        };
-        const takedPhoto = await cameraRef.current.takePictureAsync(options);
-
-        setPhoto(takedPhoto);
-    }
-  }; 
-
-  const handleRetakePhoto = () => setPhoto(null);
-
-  if (photo) return <PhotoPreviewSection photo={photo} handleRetakePhoto={handleRetakePhoto} />
-
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <AntDesign name='retweet' size={44} color='black' />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
-            <AntDesign name='camera' size={44} color='black' />
-          </TouchableOpacity>
-        </View>
-      </CameraView>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}></CameraView>
     </View>
   );
 }
@@ -68,21 +33,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-    marginHorizontal: 10,
-    backgroundColor: 'gray',
-    borderRadius: 10,
+    height: 500,
+    width: 500
   },
   text: {
     fontSize: 24,
--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,26 @@
 import React from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
+import Camera from "./camera";
 
 export default function Index() {
   const router = useRouter(); // Verwende den Router-Hook, um zu navigieren
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hallo Balllo.</Text>
-
-      {/* Button zum Navigieren zur Kamera-Seite */}
-      <Button
-        title="Go to Camera"
-        onPress={() => router.push("/camera")}  // Navigiere zur "camera"-Seite
-      />
+      <Camera />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: 180,
+    alignItems: "center"
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 20,
-  },
+  }
 });
